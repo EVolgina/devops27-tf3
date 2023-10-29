94a8c729a03577aa1ffbd5cea7ba5cca6bbd1579
EVolgina <123949683+EVolgina@users.noreply.github.com>
2023-10-29 15:38:27 +0500

    Update ansible.tf

--- ansible.tf
+++ ansible.tf
@@ -1,36 +1,41 @@
-locals {
-  databases_list = join("\n", var.databases)
-  storage_list   = join("\n", var.storage)
-}
-variable "webservers" {
-  type        = list(string)
-  description = "List of webservers"
-}
+resource "local_file" "inventory_cfg" {
+  content = templatefile("${path.module}/inventory.tftpl",
+        {
+        webservers      = yandex_compute_instance.web,
+        fe_instance   = yandex_compute_instance.fe_instance,
+        stor_instance = [yandex_compute_instance.storage]
+        }
+  )
 
-variable "storage" {
-  type        = list(string)
-  description = "List of storage instances"
+  filename = "${abspath(path.module)}/inventory"
 }
 
-resource "local_file" "ansible_inventory" {
-  content  = templatefile("${path.module}/ansible_inventory.tpl", {
-    webservers = join("\n", var.webservers)
-    databases  = join("\n", var.databases)
-    storage    = join("\n", var.storage)
-  })
-  filename = "${path.module}/ansible_inventory"
+resource "null_resource" "web_hosts_provision" {
 
-  depends_on = [
-    yandex_compute_disk.data_disk
-  ]
-}
+#Ждем создания инстанса
+depends_on = [yandex_compute_instance.storage, local_file.inventory_cfg]
 
-data "template_file" "ansible_inventory_tpl" {
-  template = file("${path.module}/ansible_inventory.tpl")
-  vars = {
-    webservers = join("\n", var.webservers)
-    databases  = join("\n", var.databases)
-    storage    = join("\n", var.storage)
+#Добавление ПРИВАТНОГО ssh ключа в ssh-agent
+  /*provisioner "local-exec" {
+        command = "cat ~/.ssh/id_ed25519 | ssh-add -"
   }
-}
+*/
+# Даем ВМ время на первый запуск. Лучше выполнить это через wait_for port 22 на стор>
+ provisioner "local-exec" {
+        command = "sleep 90"
+  }
 
+#Запуск ansible-playbook
+  provisioner "local-exec" {
+        command  = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${abspath(path.mod>
+        on_failure = continue #Продолжить выполнение terraform pipeline в случае ошибок
+        environment = { ANSIBLE_HOST_KEY_CHECKING = "False" }
+        #срабатывание триггера при изменении переменных
+  }
+        triggers = {
+        always_run      = "${timestamp()}" #всегда т.к. дата и время постоянно изменяются
+        playbook_src_hash  = file("${abspath(path.module)}/test.yml") # при изменении содержимого>
+        ssh_public_key  = local.ssh # при изменении переменной
+        }
+
+}
